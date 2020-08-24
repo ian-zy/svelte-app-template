46de1f454507dc61ac4321b8fedbc2aeb4dc9cd8
Ian Zhang <1217021+ian-zy@users.noreply.github.com>
2020-08-24 20:37:42 +0200

    use custom svelte plugin

---
+++ my-svelte-plugin.js
@@ -0,0 +1,73 @@
+const svelte = require("svelte/compiler");
+const svelteRollupPlugin = require("rollup-plugin-svelte");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function plugin(snowpackConfig, pluginOptions = {}) {
+  // Support importing Svelte files when you install dependencies.
+  snowpackConfig.installOptions.rollup.plugins.push(
+    svelteRollupPlugin({
+      include: "**/node_modules/**",
+      // this line is added so that svelte NPM modules can work correctly in dev mode
+      dev: process.env.NODE_ENV !== "production",
+    })
+  );
+
+  let svelteOptions;
+  let preprocessOptions;
+  const userSvelteConfigLoc = path.join(process.cwd(), "svelte.config.js");
+  if (fs.existsSync(userSvelteConfigLoc)) {
+    const userSvelteConfig = require(userSvelteConfigLoc);
+    const { preprocess, ..._svelteOptions } = userSvelteConfig;
+    preprocessOptions = preprocess;
+    svelteOptions = _svelteOptions;
+  }
+  // Generate svelte options from user provided config (if given)
+  svelteOptions = {
+    dev: process.env.NODE_ENV !== "production",
+    css: false,
+    ...svelteOptions,
+    ...pluginOptions,
+  };
+
+  return {
+    name: "@snowpack/plugin-svelte",
+    resolve: {
+      input: [".svelte"],
+      output: [".js", ".css"],
+    },
+    knownEntrypoints: ["svelte/internal"],
+    async load({ filePath }) {
+      let codeToCompile = fs.readFileSync(filePath, "utf-8");
+      // PRE-PROCESS
+      if (preprocessOptions) {
+        codeToCompile = (
+          await svelte.preprocess(codeToCompile, preprocessOptions, {
+            filename: filePath,
+          })
+        ).code;
+      }
+      // COMPILE
+      const { js, css } = svelte.compile(codeToCompile, {
+        ...svelteOptions,
+        outputFilename: filePath,
+        filename: filePath,
+      });
+
+      const { sourceMaps } = snowpackConfig.buildOptions;
+      const output = {
+        ".js": {
+          code: js.code,
+          map: sourceMaps ? js.map : undefined,
+        },
+      };
+      if (!svelteOptions.css && css && css.code) {
+        output[".css"] = {
+          code: css.code,
+          map: sourceMaps ? css.map : undefined,
+        };
+      }
+      return output;
+    },
+  };
+};
